d82dad084290efa2c7697a7c8be124910acd4a89
Pavel Arabei <Ptashkaaaaaa@gmail.com>
2023-02-09 15:58:24 +0100

    add remove message

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+//import { doc } from 'prettier';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
-import { Message } from './entities/message.entity';
+import { Message, MessageDocument } from './schemas/message.scheme';
 
 @Injectable()
 export class MessagesService {
+  constructor(
+    @InjectModel(Message.name) private messageModel: Model<MessageDocument>,
+  ) {}
   messages: Message[] = [{ name: 'Pavel', text: 'hey' }];
   clientIdObj = {};
 
@@ -18,12 +24,18 @@ export class MessagesService {
     return this.clientIdObj[clientId];
   }
 
-  create(createMessageDto: CreateMessageDto, clientId: string) {
+  async create(createMessageDto: CreateMessageDto, clientId: string) {
+    //const newMessage = await new this.messageModel(createMessageDto);
+    //console.log(newMessage);
+
+    //const aLLusers = await this.messageModel.find().exec();
+
     const message = {
       name: this.clientIdObj[clientId],
       text: createMessageDto.text,
     };
-    this.messages.push(createMessageDto);
+    this.messages.push(message);
+
     return message;
   }
 
@@ -36,7 +48,15 @@ export class MessagesService {
     return `This action updates a #${id} message`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} message`;
+  remove(clientId: string, text: string) {
+    const clientName = this.getClientByName(clientId);
+
+    const Index = this.messages.findIndex(
+      (message) => message.name === clientName && message.text === text,
+    );
+    if (Index === -1) return -1;
+
+    this.messages.splice(Index, 1);
+    return this.messages;
   }
 }
